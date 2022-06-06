f3448015d6ba665646070fc6b7a5df6e37284e0f
PhumzileNkosi <phumzile.pp3@gmail.com>
2022-06-06 20:25:17 +0200

    Finalised the db script

--- database/DBCreation.sql
+++ database/DBCreation.sql
@@ -1,5 +1,29 @@
-CREATE TABLE IF NOT EXISTS. Coffee (
-  CoffeeID int NOT NULL,
+USE master;
+GO
+
+CREATE DATABASE COFFEE_CONNOISSEUR_DB;
+GO
+
+USE COFFEE_CONNOISSEUR_DB;
+GO
+
+CREATE TABLE  Difficulty (
+	DifficultyID int NOT NULL,
+	DifficultyLevel varchar(250) NOT NULL,
+	PRIMARY KEY(DifficultyID)
+);
+CREATE TABLE Recipe (
+	RecipeID INT IDENTITY(1,1) PRIMARY KEY,
+	Name VARCHAR(150) NOT NULL,
+	Description VARCHAR(255),
+	Instructions VARCHAR(255),
+	PrepTime FLOAT,
+	Difficulty INT,
+	FOREIGN KEY (Difficulty) REFERENCES Difficulty(DifficultyID)
+);
+
+CREATE TABLE Coffee (
+  CoffeeID int  IDENTITY(1,1)  NOT NULL,
   RecipeID int NOT NULL,
   Name varchar(50) NOT NULL,
   Description varchar(255),
@@ -8,14 +32,14 @@ CREATE TABLE IF NOT EXISTS. Coffee (
   FOREIGN KEY (RecipeID) REFERENCES Recipe(RecipeID)
 );
 
-CREATE TABLE IF NOT EXISTS. Tag (
+CREATE TABLE  Tag (
   TagID int NOT NULL,
   Name varchar(100) NOT NULL,
   Description varchar(255),
   PRIMARY KEY(TagID)
 );
 
-CREATE TABLE IF NOT EXISTS. CoffeeTag (
+CREATE TABLE  CoffeeTag (
   CoffeeID int NOT NULL,
   TagID int NOT NULL,
   Value varchar(50) NOT NULL,
@@ -24,14 +48,22 @@ CREATE TABLE IF NOT EXISTS. CoffeeTag (
   FOREIGN KEY (TagID) REFERENCES Tag(TagID)
 );
 
-CREATE TABLE IF NOT EXISTS. Unit (
+CREATE TABLE Unit (
 	UnitID int NOT NULL,
 	UnitName int NOT NULL, 
 	PRIMARY KEY(UnitID)
 );
 
-CREATE TABLE IF NOT EXISTS. Difficulty (
-	DifficultyID int NOT NULL,
-	DifficultyLevel varchar(250) NOT NULL,
-	PRIMARY KEY(DifficultyID)
+
+CREATE TABLE Ingredient (
+ IngredientID INT IDENTITY(1,1) PRIMARY KEY ,
+ Name VARCHAR(150) NOT NULL,
 );
+CREATE TABLE RecipeIngredient (
+RecipeID INT  FOREIGN KEY REFERENCES Recipe(RecipeID),
+IngredientID INT FOREIGN KEY REFERENCES Ingredient(IngredientID),
+Quantity VARCHAR(120),
+Unit INT REFERENCES Unit(UnitID),
+PRIMARY KEY (IngredientID, RecipeID)
+);
+--IF NOT EXISTS. Tag
