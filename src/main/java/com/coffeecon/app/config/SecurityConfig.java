9d29b03da5efdb9a5661a848ad795244e5da25ad
Razeen <razeenb@bbd.co.za>
2022-06-08 11:38:08 +0200

    configured spring oAuth to use cognito

---
+++ src/main/java/com/coffeecon/app/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.coffeecon.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig  {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+
+
+        http.cors().and().csrf().disable()
+                .authorizeRequests(authz -> authz.antMatchers("/")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+
+                .oauth2ResourceServer().jwt();
+        http.requiresChannel().anyRequest().requiresSecure();
+
+
+
+
+        return http.build();
+    }
+
+
+}
--- src/main/java/com/coffeecon/app/controllers/CoffeeController.java
+++ src/main/java/com/coffeecon/app/controllers/CoffeeController.java
@@ -4,10 +4,16 @@ import com.coffeecon.app.Models.Coffee;
 import com.coffeecon.app.Models.Ingredient;
 import com.coffeecon.app.Models.Recipe;
 import com.coffeecon.app.Services.CoffeeService;
+import com.coffeecon.app.Utilities.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +34,13 @@ public class CoffeeController {
 
     @RequestMapping(value="/example", method = RequestMethod.GET)
     public ResponseEntity<Coffee> test() {
+
+
+
+
+
+
+
 
         Ingredient ingredient1 = new Ingredient(1,"milk","400","ml");
         Ingredient ingredient2 = new Ingredient(2,"sugar","2","tsp");
