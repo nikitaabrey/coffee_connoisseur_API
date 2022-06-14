0c48cad5d91403bdb142d8eb1f3ac6739518d8ce
nikitasmal <99259115+nikitasmal@users.noreply.github.com>
2022-06-14 09:11:22 +0200

    updated tags and ingredient fields

--- src/main/java/com/coffeecon/app/Repositories/CoffeeRepository.java
+++ src/main/java/com/coffeecon/app/Repositories/CoffeeRepository.java
@@ -2,6 +2,7 @@ package com.coffeecon.app.Repositories;
 import com.coffeecon.app.Models.Coffee;
 import com.coffeecon.app.Mappers.CoffeeRowMapper;
 
+import com.coffeecon.app.Models.Ingredient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
@@ -16,6 +17,8 @@ public class CoffeeRepository implements ICoffeeRepository {
     private JdbcTemplate jdbcTemplate;
 
     private final String GET_COFFEES_QUERY = "SELECT * FROM CoffeeRecipeView";
+    private final String GET_COFFEE_INGREDIENTS_QUERY = "CALL GetCoffeeIngredients(?)";
+    private final String GET_COFFEE_TAGS_QUERY = "CALL GetCoffeeTags(?)";
     private final String GET_COFFEE_DIFFICULTIES = "SELECT Recipe.RecipeID, Recipe.Name, Recipe.Description, Recipe.Instructions, Recipe.PrepTime, Recipe.DifficultyId, Coffee.CoffeeID, Coffee.Name, Coffee.Description, Coffee.RecipeID, Coffee.Rating, Difficulty.Level FROM Recipe INNER JOIN Coffee ON Recipe.RecipeID = Coffee.RecipeID INNER JOIN Difficulty ON Recipe.DifficultyID = Difficulty.DifficultyID WHERE Difficulty.Level = ?";
     private final String UPDATE_COFFEE_RATING = "UPDATE Coffee SET Rating = ? WHERE CoffeeID = ?";
 
@@ -43,6 +46,17 @@ public class CoffeeRepository implements ICoffeeRepository {
     @Override
     public List<Coffee> getByDifficulty(int level) {
         List<Coffee> coffees = jdbcTemplate.query(GET_COFFEE_DIFFICULTIES, new CoffeeRowMapper(), level);
+
+        for( int i = 0; i < coffees.size(); i++){
+            int coffeeId = coffees.get(i).getCoffeeID();
+
+            List<Tag> tags = jdbcTemplate.query(GET_COFFEE_TAGS_QUERY, new TagRowMapper(), new Object[] {coffeeId});
+            List<Ingredient> ingredients = jdbcTemplate.query(GET_COFFEE_INGREDIENTS_QUERY,new IngredientRowMapper(), new Object[] {coffeeId});
+
+            coffees.get(i).setTags(tags);
+            coffees.get(i).getRecipe().setIngredients(ingredients);
+        }
+
         return coffees;
     }
 
